dca9e26f43f4da3992a9509fa57f11acbd2b6e66
Coleby Kent <coleby210@gmail.com>
2016-05-30 23:08:16 -0700

    Initialize board and format it into a proper board

--- sudoku.rb
+++ sudoku.rb
@@ -1,2 +1,10 @@
 class Sudoku
+  def initialize(board_string)
+    @board_string = board_string
+    @board = format_board
+  end
+
+  def format_board(board_string = @board_string)
+    board_string.chars.each_slice(9).to_a
+  end
 end
