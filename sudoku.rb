2d8e76e18d0ced9c26843b6514541e7748dff2e5
Coleby Kent <coleby210@gmail.com>
2016-05-30 23:09:38 -0700

    Add to_s method to properly display board

--- sudoku.rb
+++ sudoku.rb
@@ -7,4 +7,12 @@ class Sudoku
   def format_board(board_string = @board_string)
     board_string.chars.each_slice(9).to_a
   end
+
+  def to_s
+    new_string = ""
+    @board.each do |row|
+      new_string << row.join(" ") + "\n"
+    end
+    return new_string
+  end
 end
